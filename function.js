01987a691ed9c11ab88aeb75da26acf17af7f717
prius2055 <prius2055@gmail.com>
2023-03-20 15:15:46 +0100

    Add javascript logic

--- function.js
+++ function.js
@@ -1,6 +1,20 @@
 const booksArray = [
-    {
-        title: '',
-        author: ''
-    }
-]
+  {
+    title: "",
+    author: "",
+  },
+];
+
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const addButton = document.querySelector(".btn-add");
+
+title.addEventListener("change", (e) => {
+  const tltleInput = e.target.value;
+});
+
+author.addEventListener("change", (e) => {
+  const authorInput = e.target.value;
+});
+
+addButton.addEventListener("click", () => {});
